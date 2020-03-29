d21bb667ae529fa39e6158f49bf299cf2ed6e9ef
xinding <13661957798@163.com>
2020-03-29 14:37:40 +0800

    init

---
+++ src/main/java/xinding/community/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package xinding.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by xinding on 2020/03/29.
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+
+
+    }
+
+}
