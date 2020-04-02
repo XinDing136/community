30492fc7eb2a97de28d1a025f121329f22493397
xinding <13661957798@163.com>
2020-04-02 22:49:54 +0800

    2020/4/2

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 ##社区
 
-##参考
-[Spring�ĵ�]https://spring.io/guides/gs/serving-web-content/
+##参考资料
+[Spring]https://spring.io/guides/gs/serving-web-content/
 https://elasticsearch.cn/
+
 [github app]https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
+[maven]https://www.google.com/search?q=mvnrepository&oq=mvnrepo&aqs=chrome.1.69i57j0l7.7355j0j4&sourceid=chrome&ie=UTF-8
 ##工具
+[okhttp]https://square.github.io/okhttp/
+
+## IDEA快捷键
+ command +option +L：格式化代码
+ command +shift + enter:加分号
+ shift +enter:自动跳转下一行
+ command + shift +F12 :隐藏/展开文件树
+ command + n ：快速生成代码
+ option + enter :导包，自动修正代码，解决出现的任何问题，也可以生成返回值
--- src/main/java/xinding/community/community/controller/AuthorizeController.java
+++ src/main/java/xinding/community/community/controller/AuthorizeController.java
@@ -1,17 +1,30 @@
 package xinding.community.community.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import xinding.community.community.dto.AccessTokenDTO;
+import xinding.community.community.provider.GithubProvider;
 
 /**
  *哈哈哈
  */
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id("Iv1.506b688f12bfc606");
+        accessTokenDTO.setClient_secret("c16f454debc266b39d5558dd855cc8cdae77e39c");
+        githubProvider.getAccessToken(accessTokenDTO);
+
         return "index";
     }
 
